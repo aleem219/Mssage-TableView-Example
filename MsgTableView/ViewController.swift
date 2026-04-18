2c829f773dd162b5bfc07c94f7ba326f451b0624
Your GitHub aleem219 <Your GitHub qazialeem21@gmail.com>
2026-04-18 13:25:33 +0530

    Updated Code

--- MsgTableView/ViewController.swift
+++ MsgTableView/ViewController.swift
@@ -9,20 +9,36 @@ import UIKit
 import Combine
 
 class ViewController: UIViewController {
+    @IBOutlet weak var bottomView: UIView!
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var btnSend: UIButton!
+    @IBOutlet weak var bottomViewHeightConst: NSLayoutConstraint!
+    @IBOutlet weak var msgTextView: UITextView!
+    
     private var cancellables = Set<AnyCancellable>()
     override func viewDidLoad() {
         super.viewDidLoad()
         registerTableCell()
         tableView.rowHeight = UITableView.automaticDimension
         observeAppActiveState()
+        setMsgTextView()
+    }
+    
+    
+    @IBAction func btnSendAction(_ sender: UIButton) {
+        let isPlaceholder = msgTextView.textColor == UIColor.lightGray
+        let isEmpty = msgTextView.text.trimmingCharacters(in: .whitespaces).isEmpty
+        print(isPlaceholder || isEmpty ? "Please enter your msg" : "msg has been sent")
+       handleMsgTextViewAfterSend()
     }
 }
 
 extension ViewController {
     
-    func refreshTable() {
-        self.tableView.reloadData()
+    func registerTableCell() {
+        tableView.showsVerticalScrollIndicator = false
+        self.tableView.register(UINib(nibName: "MsgCell", bundle: nil), forCellReuseIdentifier: "MsgCell")
+        self.tableView.register(UINib(nibName: "RightCell", bundle: nil), forCellReuseIdentifier: "RightCell")
     }
     
     func observeAppActiveState() {
@@ -34,10 +50,38 @@ extension ViewController {
             .store(in: &cancellables)
     }
     
-    func registerTableCell() {
-        tableView.showsVerticalScrollIndicator = false
-        self.tableView.register(UINib(nibName: "MsgCell", bundle: nil), forCellReuseIdentifier: "MsgCell")
-        self.tableView.register(UINib(nibName: "RightCell", bundle: nil), forCellReuseIdentifier: "RightCell")
+    func refreshTable() {
+        bottomView.layer.cornerRadius = bottomView.frame.height / 2
+        btnSend.layer.cornerRadius = btnSend.frame.height / 2
+        btnSend.layer.borderWidth = 1
+        btnSend.layer.borderColor = UIColor(named: "#008000")?.withAlphaComponent(0.1).cgColor
+        self.tableView.reloadData()
+    }
+    
+    func setMsgTextView() {
+        msgTextView.isScrollEnabled = false
+        msgTextView.delegate = self
+        msgTextView.text = "Message"
+        msgTextView.textColor = UIColor.lightGray
+        
+        DispatchQueue.main.async {
+            self.centerTextVertically(self.msgTextView)
+        }
+    }
+    
+    func handleMsgTextViewAfterSend() {
+        msgTextView.text = "Message"
+        msgTextView.textColor = UIColor.lightGray
+        msgTextView.isScrollEnabled = false
+        bottomViewHeightConst.constant = 45
+        msgTextView.resignFirstResponder()
+        UIView.animate(withDuration: 0.2) {
+            self.view.layoutIfNeeded()
+        }
+        
+        DispatchQueue.main.async {
+            self.centerTextVertically(self.msgTextView)
+        }
     }
 }
 
@@ -49,7 +93,7 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 4
+        return 20
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -74,3 +118,43 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
     }
 }
 
+extension ViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        if textView.textColor == UIColor.lightGray {
+            textView.text = ""
+            textView.textColor = UIColor.black
+            textView.contentInset = .zero
+        }
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        if textView.text.trimmingCharacters(in: .whitespaces).isEmpty {
+            textView.text = "Placeholder text goes right here..."
+            textView.textColor = UIColor.lightGray
+            centerTextVertically(textView)
+        }
+    }
+    
+    func centerTextVertically(_ textView: UITextView) {
+        let fittingSize = textView.sizeThatFits(CGSize(width: textView.frame.width, height: CGFloat.greatestFiniteMagnitude))
+        let topOffset = max(0, (textView.bounds.height - fittingSize.height) / 2)
+        textView.contentInset = UIEdgeInsets(top: topOffset, left: 0, bottom: 0, right: 0)
+    }
+    
+    func textViewDidChange(_ textView: UITextView) {
+        let fittingSize = textView.sizeThatFits(CGSize(width: textView.frame.width, height: CGFloat.greatestFiniteMagnitude))
+        
+        let minHeight: CGFloat = 45
+        let maxHeight: CGFloat = 200
+        
+        let newHeight = min(max(fittingSize.height + 20, minHeight), maxHeight)
+        
+        bottomViewHeightConst.constant = newHeight
+        textView.isScrollEnabled = newHeight >= maxHeight
+        
+        UIView.animate(withDuration: 0.2) {
+            self.view.layoutIfNeeded()
+        }
+    }
+}

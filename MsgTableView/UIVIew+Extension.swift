7de9a48e849ee6e849fa5c54829106ea88a966b4
Your GitHub aleem219 <Your GitHub qazialeem21@gmail.com>
2026-07-22 11:17:04 +0530

    updated code

--- MsgTableView/UIVIew+Extension.swift
+++ MsgTableView/UIVIew+Extension.swift
@@ -18,14 +18,26 @@ extension UIView {
         let width  = self.bounds.width
         let height = self.bounds.height
         let color  = self.backgroundColor?.cgColor ?? UIColor.red.cgColor
+
+       
+        let tailWidth: CGFloat = 7
+        let tailDip: CGFloat = 12
+
         let path = UIBezierPath()
         path.move(to: CGPoint(x: radius, y: 0))
         path.addLine(to: CGPoint(x: width - radius, y: 0))
         path.addQuadCurve(to: CGPoint(x: width, y: radius), controlPoint: CGPoint(x: width, y: 0))
-        path.addLine(to: CGPoint(x: width, y: height - 14))
+        path.addLine(to: CGPoint(x: width, y: height - tailDip - 4))
 
-        path.addCurve(to: CGPoint(x: width + 6, y: height - 2), controlPoint1: CGPoint(x: width, y: height - 6), controlPoint2: CGPoint(x: width + 6, y: height - 8))
-        path.addCurve(to: CGPoint(x: width - 8, y: height), controlPoint1: CGPoint(x: width + 6, y: height + 2), controlPoint2: CGPoint(x: width, y: height + 2))
+   
+        path.addCurve(to: CGPoint(x: width + tailWidth, y: height - 2),
+                      controlPoint1: CGPoint(x: width, y: height - 4),
+                      controlPoint2: CGPoint(x: width + tailWidth, y: height - tailDip * 0.4))
+
+
+        path.addCurve(to: CGPoint(x: width - tailWidth - 6, y: height),
+                      controlPoint1: CGPoint(x: width + tailWidth, y: height + 1),
+                      controlPoint2: CGPoint(x: width - 2, y: height + 2))
 
         path.addLine(to: CGPoint(x: radius, y: height))
         path.addQuadCurve(to: CGPoint(x: 0, y: height - radius), controlPoint: CGPoint(x: 0, y: height))
@@ -51,6 +63,11 @@ extension UIView {
         let width  = self.bounds.width
         let height = self.bounds.height
         let color  = self.backgroundColor?.cgColor ?? UIColor.white.cgColor
+
+
+        let tailWidth: CGFloat = 7
+        let tailDip: CGFloat = 12
+
         let path = UIBezierPath()
         path.move(to: CGPoint(x: radius, y: 0))
         path.addLine(to: CGPoint(x: width - radius, y: 0))
@@ -58,9 +75,17 @@ extension UIView {
 
         path.addLine(to: CGPoint(x: width, y: height - radius))
         path.addQuadCurve(to: CGPoint(x: width - radius, y: height), controlPoint: CGPoint(x: width, y: height))
-        path.addLine(to: CGPoint(x: 8, y: height))
-        path.addCurve(to: CGPoint(x: -6, y: height - 2), controlPoint1: CGPoint(x: 0,  y: height + 2), controlPoint2: CGPoint(x: -6, y: height + 2))
-        path.addCurve(to: CGPoint(x: 0,  y: height - 14), controlPoint1: CGPoint(x: -6, y: height - 8), controlPoint2: CGPoint(x: 0,  y: height - 6))
+        path.addLine(to: CGPoint(x: tailWidth + 6, y: height))
+
+
+        path.addCurve(to: CGPoint(x: -tailWidth, y: height - 2),
+                      controlPoint1: CGPoint(x: 2, y: height + 2),
+                      controlPoint2: CGPoint(x: -tailWidth, y: height + 1))
+
+
+        path.addCurve(to: CGPoint(x: 0, y: height - tailDip - 4),
+                      controlPoint1: CGPoint(x: -tailWidth, y: height - tailDip * 0.4),
+                      controlPoint2: CGPoint(x: 0, y: height - 4))
 
         path.addLine(to: CGPoint(x: 0, y: radius))
         path.addQuadCurve(to: CGPoint(x: radius, y: 0), controlPoint: CGPoint(x: 0, y: 0))
